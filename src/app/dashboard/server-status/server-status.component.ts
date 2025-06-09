c88fc96bd83230237cb6885cc15b751688ddb320
Šárka F. <sarka.flidrova@email.cz>
2025-06-09 16:24:55 +0200

    Add reusable component dashboard item

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server-status',
   standalone: true,
-  imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
