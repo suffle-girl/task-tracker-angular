1e25f8ecbe904b0767daf899ab191895210842b9
Šárka F. <sarka.flidrova@email.cz>
2025-06-09 15:26:55 +0200

    Add tickets support component and fix styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficComponent } from './traffic/traffic.component';
+import { TicketsSupportComponent } from './tickets-support/tickets-support.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsSupportComponent,
+  ],
 })
 export class AppComponent {}
---
+++ src/app/tickets-support/tickets-support.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-support',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickets-support.component.html',
+  styleUrl: './tickets-support.component.css'
+})
+export class TicketsSupportComponent {
+
+}
