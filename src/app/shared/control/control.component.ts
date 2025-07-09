d0431ec9508c2117f1fd7381b883c7ec70190185
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-09 15:40:41 +0200

    Handling host element with HostBinding and HostListener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,10 +13,19 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  host: {
-    class: 'control',
-  },
+  // host: {
+  //   class: 'control',
+  //   '(click)': 'onClick()',
+  // },
 })
 export class ControlComponent {
+  @HostBinding('class') className = 'control';
+  @HostListener('click') onClick() {
+    console.log('clicked!');
+  }
   label = input.required<string>();
+
+  // onClick() {
+  //   console.log('clicked!');
+  // }
 }
