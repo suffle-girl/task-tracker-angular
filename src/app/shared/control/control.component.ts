a03eec6f99b92db934e04dcdf1d233d7311f1cf4
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-09 15:55:15 +0200

    inject ElementRef to access it programmatically

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
-  HostBinding,
-  HostListener,
+  ElementRef,
+  // HostBinding,
+  // HostListener,
+  inject,
   input,
   ViewEncapsulation,
 } from '@angular/core';
@@ -13,19 +15,21 @@ import {
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
-  // host: {
-  //   class: 'control',
-  //   '(click)': 'onClick()',
-  // },
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
-  @HostBinding('class') className = 'control';
-  @HostListener('click') onClick() {
-    console.log('clicked!');
-  }
-  label = input.required<string>();
-
-  // onClick() {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
   //   console.log('clicked!');
   // }
+  label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked!');
+    console.log(this.el);
+  }
 }
