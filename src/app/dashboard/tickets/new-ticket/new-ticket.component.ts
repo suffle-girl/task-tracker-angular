61cd269f28140889c76c4da466a8d16626078ffd
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-10 15:24:59 +0200

    viewChild as a signal function

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,12 +11,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: HTMLInputElement, textAreaText: string) {
     const enteredTitle = title.value;
     console.log('entered title: ', enteredTitle);
     console.log('entered text: ', textAreaText);
-    this.form?.nativeElement.reset();
+    // this.form()?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
