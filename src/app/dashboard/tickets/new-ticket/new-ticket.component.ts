1ed989a3eb6b18b22e26d0e395c9dd0ba9ea66d7
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-10 13:06:36 +0200

    template variables to retrieve input data from the user

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  onSubmit(title: HTMLInputElement, textAreaText: string) {
+    const enteredTitle = title.value;
+    console.log('entered title: ', enteredTitle);
+    console.log('entered text: ', textAreaText);
+  }
+}
