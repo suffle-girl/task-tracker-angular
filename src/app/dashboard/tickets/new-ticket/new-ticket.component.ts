036f859e2c964c5d1ca8fd9f2a1cd0e55938d416
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-11 09:24:59 +0200

    decorator based queries and lifecycle hooks - ngAfterViewInit and ngAfterContentInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -10,16 +17,26 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+export class NewTicketComponent implements AfterViewInit, OnInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   // private form = viewChild<ElementRef<HTMLFormElement>>('form');
-  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log('on init');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('after view init');
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(title: HTMLInputElement, textAreaText: string) {
     const enteredTitle = title.value;
     console.log('entered title: ', enteredTitle);
     console.log('entered text: ', textAreaText);
-    // this.form()?.nativeElement.reset();
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
+    // this.form().nativeElement.reset();
   }
 }
